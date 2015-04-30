20d9050f8644099f135d17772861652f1dd3a597
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-04-30 20:14:03 +0200

    Update #5

---
+++ Bellows/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,69 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var M_Apropos = {};	// @menuItem
+	var menuItem4 = {};	// @menuItem
+	var W_Login = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		var vUser = WAF.directory.currentUser();
+		if (vUser === null) {
+			$$('main').destroy();
+		 } else {
+		 	
+			if (waf.directory.currentUserBelongsTo("Ingénieur")) {
+				$$('M_Administration').hide();
+			}
+			
+		 }
+
+	};// @lock
+
+	M_Apropos.click = function M_Apropos_click (event)// @startlock
+	{// @endlock
+		$$("cchg").show();
+		$$('component1').loadComponent("/Apropos.waComponent");
+
+	};// @lock
+
+	menuItem4.click = function menuItem4_click (event)// @startlock
+	{// @endlock
+		$$("cchg").show();
+		$$('component1').loadComponent("/Gest_Utilisateurs.waComponent");
+
+	};// @lock
+
+	W_Login.logout = function W_Login_logout (event)// @startlock
+	{// @endlock
+		$$('main').destroy();
+	};// @lock
+
+	W_Login.login = function W_Login_login (event)// @startlock
+	{// @endlock
+		var vUser, vGroups, curSession, theUser; 
+		
+		vUser = WAF.directory.currentUser();
+		// alert(vUser.fullName+" " + vUser.ID + " "+ vUser.userName);
+		if (vUser !== null) {
+			
+			window.location = '/index.html';
+			
+			
+		}
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("M_Apropos", "click", M_Apropos.click, "WAF");
+	WAF.addListener("menuItem4", "click", menuItem4.click, "WAF");
+	WAF.addListener("W_Login", "logout", W_Login.logout, "WAF");
+	WAF.addListener("W_Login", "login", W_Login.login, "WAF");
+// @endregion
+};// @endlock
