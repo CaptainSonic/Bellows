1eeea77cfd70b7e43aca360be3d2cc70e5819f14
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-05-08 16:15:01 +0200

    Update #32

--- Bellows/WebFolder/Compare_Bellows.waComponent/Compare_Bellows.js
+++ Bellows/WebFolder/Compare_Bellows.waComponent/Compare_Bellows.js
@@ -14,8 +14,38 @@ function constructor (id) {
 
 	$$("cchg").hide();
 	$$("component1_ListBell").setRowHeight(22);
+	$$('component1_slParois').addHandle(30);
+	$$('component1_slOndes').addHandle(40);
+	$$('component1_slDiamCollet1').addHandle(130);	
+	$$('component1_slDiamCollet2').addHandle(130);	
+	$$('component1_slExt').addHandle(130);	
+	$$('component1_slInt').addHandle(130);	
+	$$('component1_slL2').addHandle(130);	
+	$$('component1_slL3').addHandle(130);	
+
 	
 	// @region namespaceDeclaration// @startlock
+	var btShow = {};	// @buttonImage
+	var btReset = {};	// @buttonImage
+	var slL3 = {};	// @slider
+	var slL2 = {};	// @slider
+	var slInt = {};	// @slider
+	var slExt = {};	// @slider
+	var cbxL3 = {};	// @checkbox
+	var cbxL2 = {};	// @checkbox
+	var cbxInt = {};	// @checkbox
+	var cbxExt = {};	// @checkbox
+	var slDiamCollet2 = {};	// @slider
+	var slDiamCollet1 = {};	// @slider
+	var cbxDiamCollet2 = {};	// @checkbox
+	var cbxDiamCollet1 = {};	// @checkbox
+	var cbxOutil = {};	// @checkbox
+	var cbxMatiere = {};	// @checkbox
+	var slOndes = {};	// @slider
+	var cbxOndes = {};	// @checkbox
+	var cbxParois = {};	// @checkbox
+	var btSearch = {};	// @buttonImage
+	var slParois = {};	// @slider
 	var Btz3 = {};	// @buttonImage
 	var Btc3 = {};	// @buttonImage
 	var Btz2 = {};	// @buttonImage
@@ -25,6 +55,444 @@ function constructor (id) {
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	btShow.click = function btShow_click (event)// @startlock
+	{// @endlock
+		$$('component1_btShow').hide();
+		$$('component1_Btc1').hide();
+		$$('component1_Btc2').hide();
+		$$('component1_Btc3').hide();
+		$$('component1_Btz1').hide();
+		$$('component1_Btz2').hide();
+		$$('component1_Btz3').hide();
+		$$('component1_ListBell').disable();
+		$$('component1_PanelSearch').show();
+		
+		
+	};// @lock
+
+	btReset.click = function btReset_click (event)// @startlock
+	{// @endlock
+		$$('component1_slInt').hide();
+		$$('component1_stgInt').hide();
+		$$('component1_stdInt').hide();
+		$$('component1_slL2').hide();
+		$$('component1_stgL2').hide();
+		$$('component1_stdL2').hide();
+		$$('component1_slL3').hide();
+		$$('component1_stgL3').hide();
+		$$('component1_stdL3').hide();
+		$$('component1_slExt').hide();
+		$$('component1_stgExt').hide();
+		$$('component1_stdExt').hide();
+		$$('component1_slDiamCollet1').hide();
+		$$('component1_stgCol1').hide();
+		$$('component1_stdCol1').hide();
+		$$('component1_slDiamCollet2').hide();
+		$$('component1_stgCol2').hide();
+		$$('component1_stdCol2').hide();
+		$$('component1_slOndes').hide();
+		$$('component1_stgOndes').hide();
+		$$('component1_stdOndes').hide();
+		$$('component1_slParois').hide();
+		$$('component1_stgParois').hide();
+		$$('component1_stdParois').hide();
+		$$('component1_cbMatiere').hide();
+		$$('component1_cbOutil').hide();
+		$$('component1_sRef').setValue(null);
+		$$('component1_sCode').setValue(null);
+		$$('component1_cbCollet1').setValue("-");
+		$$('component1_cbbCollet1').setValue("-");
+		$$('component1_cbCollet2').setValue("-");
+		$$('component1_cbbCollet2').setValue("-");
+		$$('component1_cbForm').setValue("-");
+		$$('component1_cbxInt').uncheck();
+		$$('component1_cbxL2').uncheck();
+		$$('component1_cbxL3').uncheck();
+		$$('component1_cbxExt').uncheck();
+		$$('component1_cbxInt').uncheck();
+		$$('component1_cbxDiamCollet2').uncheck();
+		$$('component1_cbxDiamCollet1').uncheck();
+		$$('component1_cbxOndes').uncheck();
+		$$('component1_cbxParois').uncheck();
+		$$('component1_cbxMatiere').uncheck();
+		$$('component1_cbxOutil').uncheck();
+		
+	};// @lock
+
+	slL3.slidechange = function slL3_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgL3').setValue(event.data.values[0]); 
+		$$('component1_stdL3').setValue(event.data.values[1]);
+	};// @lock
+
+	slL3.slide = function slL3_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgL3').setValue(event.data.values[0]); 
+		$$('component1_stdL3').setValue(event.data.values[1]);
+	};// @lock
+
+	slL2.slidechange = function slL2_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgL2').setValue(event.data.values[0]); 
+		$$('component1_stdL2').setValue(event.data.values[1]);
+	};// @lock
+
+	slL2.slide = function slL2_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgL2').setValue(event.data.values[0]); 
+		$$('component1_stdL2').setValue(event.data.values[1]);
+	};// @lock
+
+	slInt.slidechange = function slInt_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgInt').setValue(event.data.values[0]); 
+		$$('component1_stdInt').setValue(event.data.values[1]);
+	};// @lock
+
+	slInt.slide = function slInt_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgInt').setValue(event.data.values[0]); 
+		$$('component1_stdInt').setValue(event.data.values[1]);
+	};// @lock
+
+	slExt.slidechange = function slExt_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgExt').setValue(event.data.values[0]); 
+		$$('component1_stdExt').setValue(event.data.values[1]);
+	};// @lock
+
+	slExt.slide = function slExt_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgExt').setValue(event.data.values[0]); 
+		$$('component1_stdExt').setValue(event.data.values[1]);
+	};// @lock
+
+	cbxL3.click = function cbxL3_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxL3').getValue()) {
+			$$('component1_slL3').show();
+			$$('component1_stgL3').show();
+			$$('component1_stdL3').show();
+			$$('component1_slL3').setValues([10,30]);
+		} else {
+			$$('component1_slL3').hide();
+			$$('component1_stgL3').hide();
+			$$('component1_stdL3').hide();
+		}
+	};// @lock
+
+	cbxL2.click = function cbxL2_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxL2').getValue()) {
+			$$('component1_slL2').show();
+			$$('component1_stgL2').show();
+			$$('component1_stdL2').show();
+			$$('component1_slL2').setValues([10,30]);
+		} else {
+			$$('component1_slL2').hide();
+			$$('component1_stgL2').hide();
+			$$('component1_stdL2').hide();
+		}
+	};// @lock
+
+	cbxInt.click = function cbxInt_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxInt').getValue()) {
+			$$('component1_slInt').show();
+			$$('component1_stgInt').show();
+			$$('component1_stdInt').show();
+			$$('component1_slInt').setValues([5,20]);
+		} else {
+			$$('component1_slInt').hide();
+			$$('component1_stgInt').hide();
+			$$('component1_stdInt').hide();
+		}
+	};// @lock
+
+	cbxExt.click = function cbxExt_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxExt').getValue()) {
+			$$('component1_slExt').show();
+			$$('component1_stgExt').show();
+			$$('component1_stdExt').show();
+			$$('component1_slExt').setValues([10,30]);
+		} else {
+			$$('component1_slExt').hide();
+			$$('component1_stgExt').hide();
+			$$('component1_stdExt').hide();
+		}
+	};// @lock
+
+	slDiamCollet2.slidechange = function slDiamCollet2_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgCol2').setValue(event.data.values[0]); 
+		$$('component1_stdCol2').setValue(event.data.values[1]);
+	};// @lock
+
+	slDiamCollet2.slide = function slDiamCollet2_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgCol2').setValue(event.data.values[0]); 
+		$$('component1_stdCol2').setValue(event.data.values[1]);
+	};// @lock
+
+	slDiamCollet1.slidechange = function slDiamCollet1_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgCol1').setValue(event.data.values[0]); 
+		$$('component1_stdCol1').setValue(event.data.values[1]);
+	};// @lock
+
+	slDiamCollet1.slide = function slDiamCollet1_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgCol1').setValue(event.data.values[0]); 
+		$$('component1_stdCol1').setValue(event.data.values[1]);
+	};// @lock
+
+	cbxDiamCollet2.click = function cbxDiamCollet2_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxDiamCollet2').getValue()) {
+			$$('component1_slDiamCollet2').show();
+			$$('component1_stgCol2').show();
+			$$('component1_stdCol2').show();
+			$$('component1_slDiamCollet2').setValues([10,30]);
+		} else {
+			$$('component1_slDiamCollet2').hide();
+			$$('component1_stgCol2').hide();
+			$$('component1_stdCol2').hide();
+		}
+	};// @lock
+
+	cbxDiamCollet1.click = function cbxDiamCollet1_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxDiamCollet1').getValue()) {
+			$$('component1_slDiamCollet1').show();
+			$$('component1_stgCol1').show();
+			$$('component1_stdCol1').show();
+			$$('component1_slDiamCollet1').setValues([10,30]);
+		} else {
+			$$('component1_slDiamCollet1').hide();
+			$$('component1_stgCol1').hide();
+			$$('component1_stdCol1').hide();
+		}
+	};// @lock
+
+	cbxOutil.click = function cbxOutil_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxOutil').getValue()) {
+			$$('component1_cbOutil').show();
+		} else {
+			$$('component1_cbOutil').hide();
+		}
+	};// @lock
+
+	cbxMatiere.click = function cbxMatiere_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxMatiere').getValue()) {
+			$$('component1_cbMatiere').show();
+		} else {
+			$$('component1_cbMatiere').hide();
+		}
+	};// @lock
+
+	slOndes.slidechange = function slOndes_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgOndes').setValue(event.data.values[0]); 
+		$$('component1_stdOndes').setValue(event.data.values[1]);
+	};// @lock
+
+	slOndes.slide = function slOndes_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgOndes').setValue(event.data.values[0]); 
+		$$('component1_stdOndes').setValue(event.data.values[1]);
+	};// @lock
+
+	cbxOndes.click = function cbxOndes_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxOndes').getValue()) {
+			$$('component1_slOndes').show();
+			$$('component1_stgOndes').show();
+			$$('component1_stdOndes').show();
+			$$('component1_slOndes').setValues([3,18]);
+		} else {
+			$$('component1_slOndes').hide();
+			$$('component1_stgOndes').hide();
+			$$('component1_stdOndes').hide();
+		}
+	};// @lock
+
+	cbxParois.click = function cbxParois_click (event)// @startlock
+	{// @endlock
+		if ($$('component1_cbxParois').getValue()) {
+			$$('component1_slParois').show();
+			$$('component1_stgParois').show();
+			$$('component1_stdParois').show();
+			$$('component1_slParois').setValues([1,2]);
+		} else {
+			$$('component1_slParois').hide();
+			$$('component1_stgParois').hide();
+			$$('component1_stdParois').hide();
+		}
+
+	};// @lock
+
+	btSearch.click = function btSearch_click (event)// @startlock
+	{// @endlock
+		var LQuery, pRef, pCode, pMat, pOutil, pForm, p1C1, p2C1, p1C2, p2C2, pParm, pParM;
+		
+		$$('component1_btShow').show();
+		$$('component1_Btc1').show();
+		$$('component1_Btc2').show();
+		$$('component1_Btc3').show();
+		$$('component1_Btz1').show();
+		$$('component1_Btz2').show();
+		$$('component1_Btz3').show();
+		$$('component1_ListBell').enable();
+		$$('component1_ListBell').setReadOnly(true);
+		$$('component1_PanelSearch').hide();
+		
+		
+		LQuery = "Reference = :1 ";
+		if ($$('component1_sRef').getValue().length = 0) {
+			pRef = "*";
+		} else {
+			pRef = "*" + $$('component1_sRef').getValue() + "*";
+		}
+		
+				
+		if ($$('component1_sCode').getValue().length = 0) {
+			pCode = "*";
+		} else {
+			pCode = "*" + $$('component1_sCode').getValue() + "*";
+			LQuery = LQuery + "and Code = :2 ";
+		}
+		
+		pMat = "*";
+		if ($$('component1_cbxMatiere').getValue()) {
+			pMat = $$('component1_cbMatiere').getValue();
+			LQuery = LQuery + "and Matiere.ID = :3 ";
+		}
+		
+		pOutil = "*";
+		if ($$('component1_cbxOutil').getValue()) {
+			pOutil = $$('component1_cbOutil').getValue();
+			LQuery = LQuery + "and Outil.ID = :4 ";
+		}
+		
+		pForm = "*";
+		if ($$('component1_cbForm').getValue() !== "-") {
+			pForm = $$('component1_cbForm').getValue();
+			LQuery = LQuery + "and Type_Formage = :5 ";
+		}
+		
+		p1C1 = "*";
+		if ($$('component1_cbCollet1').getValue() !== "-") {
+			p1C1 = $$('component1_cbCollet1').getValue();
+			p2C1 = "*";
+			if ($$('component1_cbbCollet1').getValue() !== "-") {
+				p2C1 = $$('component1_cbbCollet1').getValue();
+				LQuery = LQuery + "and ( Collet1 = :6 or Collet1 = :7) ";
+			} else {
+				LQuery = LQuery + "and Collet1 = :6 ";
+			}
+		}
+		
+		p1C2 = "*";
+		if ($$('component1_cbCollet2').getValue() !== "-") {
+			p1C2 = $$('component1_cbCollet2').getValue();
+			p2C2 = "*";
+			if ($$('component1_cbbCollet2').getValue() !== "-") {
+				p2C2 = $$('component1_cbbCollet2').getValue();
+				LQuery = LQuery + "and ( Collet2 = :8 or Collet2 = :9) ";
+			} else {
+				LQuery = LQuery + "and Collet2 = :8 ";
+			}
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxParois').getValue()) {
+			pParm = parseInt($$('component1_stgParois').getValue(),10);
+			pParM = parseInt($$('component1_stdParois').getValue(),10);
+			//alert("Min "+ pParm + "  Max "+pParM);
+			LQuery = LQuery + "and ( Nb_Parois >= " + pParm + " and Nb_Parois <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxOndes').getValue()) {
+			pParm = $$('component1_stgOndes').getValue();
+			pParM = $$('component1_stdOndes').getValue();
+			LQuery = LQuery + "and ( Nb_Ondes >= " + pParm + " and Nb_Ondes <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxDiamCollet1').getValue()) {
+			pParm = $$('component1_stgCol1').getValue();
+			pParM = $$('component1_stdCol1').getValue();
+			LQuery = LQuery + "and ( Diam_Collet1 >= " + pParm + " and Diam_Collet1 <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxDiamCollet2').getValue()) {
+			pParm = $$('component1_stgCol2').getValue();
+			pParM = $$('component1_stdCol2').getValue();
+			LQuery = LQuery + "and ( Diam_Collet2 >= " + pParm + " and Diam_Collet2 <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxExt').getValue()) {
+			pParm = $$('component1_stgExt').getValue();
+			pParM = $$('component1_stdExt').getValue();
+			LQuery = LQuery + "and ( Ext_Onde >= " + pParm + " and Ext_Onde <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxInt').getValue()) {
+			pParm = $$('component1_stgInt').getValue();
+			pParM = $$('component1_stdInt').getValue();
+			LQuery = LQuery + "and ( Int_Onde >= " + pParm + " and Int_Onde <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxL2').getValue()) {
+			pParm = $$('component1_stgL2').getValue();
+			pParM = $$('component1_stdL2').getValue();
+			LQuery = LQuery + "and ( L2 >= " + pParm + " and L2 <= " + pParM + ") ";
+		}
+		
+		pParm = 0;
+		pParM = 0;
+		if ($$('component1_cbxL3').getValue()) {
+			pParm = $$('component1_stgL3').getValue();
+			pParM = $$('component1_stdL3').getValue();
+			LQuery = LQuery + "and ( L3 >= " + pParm + " and L3 <= " + pParM + ") ";
+		}
+		
+		//WAF.sources.component1_soufflets.query();
+		WAF.sources.component1_soufflets.query(LQuery, pRef, pCode, pMat, pOutil, pForm, p1C1, p2C1, p1C2, p2C2);
+		
+		
+		
+	};// @lock
+
+	slParois.slidechange = function slParois_slidechange (event)// @startlock
+	{// @endlock
+		$$('component1_stgParois').setValue(event.data.values[0]); 
+		$$('component1_stdParois').setValue(event.data.values[1]);
+
+	};// @lock
+
+	slParois.slide = function slParois_slide (event)// @startlock
+	{// @endlock
+		$$('component1_stgParois').setValue(event.data.values[0]); 
+		$$('component1_stdParois').setValue(event.data.values[1]);
+
+	};// @lock
 
 	Btz3.click = function Btz3_click (event)// @startlock
 	{// @endlock
@@ -207,6 +675,35 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_btShow", "click", btShow.click, "WAF");
+	WAF.addListener(this.id + "_btReset", "click", btReset.click, "WAF");
+	WAF.addListener(this.id + "_slL3", "slidechange", slL3.slidechange, "WAF");
+	WAF.addListener(this.id + "_slL3", "slide", slL3.slide, "WAF");
+	WAF.addListener(this.id + "_slL2", "slidechange", slL2.slidechange, "WAF");
+	WAF.addListener(this.id + "_slL2", "slide", slL2.slide, "WAF");
+	WAF.addListener(this.id + "_slInt", "slidechange", slInt.slidechange, "WAF");
+	WAF.addListener(this.id + "_slInt", "slide", slInt.slide, "WAF");
+	WAF.addListener(this.id + "_slExt", "slidechange", slExt.slidechange, "WAF");
+	WAF.addListener(this.id + "_slExt", "slide", slExt.slide, "WAF");
+	WAF.addListener(this.id + "_cbxL3", "click", cbxL3.click, "WAF");
+	WAF.addListener(this.id + "_cbxL2", "click", cbxL2.click, "WAF");
+	WAF.addListener(this.id + "_cbxInt", "click", cbxInt.click, "WAF");
+	WAF.addListener(this.id + "_cbxExt", "click", cbxExt.click, "WAF");
+	WAF.addListener(this.id + "_slDiamCollet2", "slidechange", slDiamCollet2.slidechange, "WAF");
+	WAF.addListener(this.id + "_slDiamCollet2", "slide", slDiamCollet2.slide, "WAF");
+	WAF.addListener(this.id + "_slDiamCollet1", "slidechange", slDiamCollet1.slidechange, "WAF");
+	WAF.addListener(this.id + "_slDiamCollet1", "slide", slDiamCollet1.slide, "WAF");
+	WAF.addListener(this.id + "_cbxDiamCollet2", "click", cbxDiamCollet2.click, "WAF");
+	WAF.addListener(this.id + "_cbxDiamCollet1", "click", cbxDiamCollet1.click, "WAF");
+	WAF.addListener(this.id + "_cbxOutil", "click", cbxOutil.click, "WAF");
+	WAF.addListener(this.id + "_cbxMatiere", "click", cbxMatiere.click, "WAF");
+	WAF.addListener(this.id + "_slOndes", "slidechange", slOndes.slidechange, "WAF");
+	WAF.addListener(this.id + "_slOndes", "slide", slOndes.slide, "WAF");
+	WAF.addListener(this.id + "_cbxOndes", "click", cbxOndes.click, "WAF");
+	WAF.addListener(this.id + "_cbxParois", "click", cbxParois.click, "WAF");
+	WAF.addListener(this.id + "_btSearch", "click", btSearch.click, "WAF");
+	WAF.addListener(this.id + "_slParois", "slidechange", slParois.slidechange, "WAF");
+	WAF.addListener(this.id + "_slParois", "slide", slParois.slide, "WAF");
 	WAF.addListener(this.id + "_Btz3", "click", Btz3.click, "WAF");
 	WAF.addListener(this.id + "_Btc3", "click", Btc3.click, "WAF");
 	WAF.addListener(this.id + "_Btz2", "click", Btz2.click, "WAF");
